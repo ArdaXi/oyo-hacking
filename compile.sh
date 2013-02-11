730d3514765496d30bbe0fb87373b0cf4aee92c8
Arien Holthuizen <arienh4@arienh4.net>
2013-02-11 23:30:58 +0100

    Set compile to use root as owner.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo Compiling root filesystem.
 cd rootfs
-tar -cvf ../qdutil/upfw/rootfs.tar *
+tar -cvf --owner=root ../qdutil/upfw/rootfs.tar *
 cd ../
 tar -pcvjf oyofw.tar.bz2 qdutil
 md5sum -b oyofw.tar.bz > oyofw.md5sums
