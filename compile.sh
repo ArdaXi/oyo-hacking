5988de673a41a469a6e11ca9af7656cc30f94f23
Arien Holthuizen <arienh4@arienh4.net>
2013-02-11 23:37:41 +0100

    Srsly.

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ cd rootfs
 tar --owner=root -cvf ../qdutil/upfw/rootfs.tar *
 cd ../
 tar -pcvjf oyofw.tar.bz2 qdutil
-md5sum -b oyofw.tar.bz22 > oyofw.md5sums
+md5sum -b oyofw.tar.bz2 > oyofw.md5sums
 echo Done.
