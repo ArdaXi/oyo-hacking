24d79f87dab1430b38a8658e8c7da0c1ae68c024
Arien Holthuizen <arienh4@arienh4.net>
2013-02-11 23:33:18 +0100

    Properly this time. xkcd.com/1168

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo Compiling root filesystem.
 cd rootfs
-tar -cvf --owner=root ../qdutil/upfw/rootfs.tar *
+tar --owner=root -cvf ../qdutil/upfw/rootfs.tar *
 cd ../
 tar -pcvjf oyofw.tar.bz2 qdutil
 md5sum -b oyofw.tar.bz > oyofw.md5sums
